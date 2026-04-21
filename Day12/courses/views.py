83476bad17f48343b32727e833b49f2dd62443d4
deepak <deepakrajesh20071@gmail.com>
2026-04-21 11:05:56 +0530

    Added Day 12 tasks

---
+++ Day12/courses/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to Klaw Courses")
+
+def detail_view(request, name):
+    return HttpResponse(f"Course Name: {name}")
+# Create your views here.
